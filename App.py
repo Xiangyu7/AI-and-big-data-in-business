527487c41dedd6e199ada6662abec9698b38a1ca
Xiangyu7 <125335888+Xiangyu7@users.noreply.github.com>
2024-11-18 11:01:57 +0000

    1

--- App.py
+++ App.py
@@ -1,1 +1,11 @@
 from flask import Flask
+from flask import render_template,request
+
+App = Flask("__name__")
+
+@App.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    App.run()
